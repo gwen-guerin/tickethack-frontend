1de2abad8984366823e366cb112c3e5e7a5ca3f3
Gwen Guérin <gwenael.guerin@protonmail.com>
2022-11-09 17:34:53 +0100

    presque

--- cart.js
+++ cart.js
@@ -25,6 +25,39 @@ window.addEventListener('load', () => {
         }
       }
     });
-    
+
   });
 
+  // function deleteCart() {
+  //   for (const cart of document.getElementsByClassName('delete')) {
+  //     cart.addEventListener('click' , () => {
+  //       fetch('http://localhost:3000/trips/cart/delete', {
+  //         method: 'DELETE',
+  //       }).then(response => response.json()).then(data => {
+  //         if (data.result) {
+	// 					this.parentNode.remove();
+	// 				}
+  //       })
+  //     })
+  //   }
+  // }
+
+  // function updateDeleteCityEventListener() {
+  //   for (let i = 0; i < document.querySelectorAll('.delete').length; i++) {
+  //     document.querySelectorAll('.delete')[i].addEventListener('click', () =>{
+  //       console.log("coucou")
+  //       console.log(trip);
+  //       const date = this.parentNode.myTime.value
+  //       const price = this.parentNode.myPrice.value
+  //       // console.log("coucou");
+  //       fetch(`http://localhost:3000/trips/delete`, 
+  //       { method: 'DELETE',
+  //       body: JSON.stringify({ trip, date, price }) })
+  //         .then(response => response.json())
+  //         .then(data => {
+  //             data.parentNode.remove();
+  //         });
+  //     });
+  //   }    
+  // }
+  // updateDeleteCityEventListener() 
